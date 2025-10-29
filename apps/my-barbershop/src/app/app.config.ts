ca1b89ec478364689660646a82716c2a9036d05c
Rodrigos-dev <rodrigos.ars.dev@gmail.com>
2025-10-29 16:17:36 -0300

    ✨ feat: add transloco lib and config

--- apps/my-barbershop/src/app/app.config.ts
+++ apps/my-barbershop/src/app/app.config.ts
@@ -3,13 +3,32 @@ import { provideNzI18n, pt_BR } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import { provideHttpClient } from '@angular/common/http';
 import pt from '@angular/common/locales/pt';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideTransloco } from '@jsverse/transloco';
 
 import { appRoutes } from './app.routes';
+import { TranslocoHttpLoader } from './transloco-loader';
 
 registerLocaleData(pt);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes), provideNzI18n(pt_BR), provideHttpClient()],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideNzI18n(pt_BR),
+    provideHttpClient(),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['pt-BR', 'en'],
+        defaultLang: 'pt-BR',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
 };
--- apps/my-barbershop/src/app/domain/auth/pages/login/login.page.ts
+++ apps/my-barbershop/src/app/domain/auth/pages/login/login.page.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'mb-login',
-  imports: [],
+  imports: [TranslocoModule],
   templateUrl: './login.page.html',
   styleUrl: './login.page.scss',
 })
-export class LoginPage {}
+export class LoginPage {
+  private readonly translocoService = inject(TranslocoService);
+
+  changeLang(lang: string) {
+    this.translocoService.setActiveLang(lang);
+  }
+}
