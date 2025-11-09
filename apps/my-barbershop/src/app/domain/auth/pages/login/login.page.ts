e8c8de87ca8ff4434ccb95bc7cc4e7788f6df7a3
Rodrigos-dev <rodrigos.ars.dev@gmail.com>
2025-11-09 20:01:39 -0300

    feat: add loading in login click

--- apps/my-barbershop/src/app/domain/auth/pages/login/login.page.ts
+++ apps/my-barbershop/src/app/domain/auth/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { injectSupabase } from 'apps/my-barbershop/src/app/shared/functions/inject-supabase.function';
+import { LoadingService } from 'apps/my-barbershop/src/app/shared/services/loading/loading.service';
 import { eDynamicField } from 'apps/my-barbershop/src/app/widget/components/dynamic-form/dynamic-field.enum';
 import { iDynamicFormConfig } from 'apps/my-barbershop/src/app/widget/components/dynamic-form/dynamic-form-config.interface';
 import { DynamicFormComponent } from 'apps/my-barbershop/src/app/widget/components/dynamic-form/dynamic-form.component';
@@ -7,6 +8,7 @@ import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { NzSpinComponent } from 'ng-zorro-antd/spin';
 
 import { Component, inject, ViewChild } from '@angular/core';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
@@ -16,7 +18,7 @@ import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'mb-login',
-  imports: [DynamicFormComponent, NzFlexModule, NzButtonComponent, NzFormModule, NzInputModule, ReactiveFormsModule, RouterModule],
+  imports: [DynamicFormComponent, NzFlexModule, NzButtonComponent, NzFormModule, NzInputModule, ReactiveFormsModule, RouterModule, NzSpinComponent],
   templateUrl: './login.page.html',
   styleUrl: './login.page.scss',
 })
@@ -25,6 +27,7 @@ export class LoginPage {
   private readonly notificationService = inject(NzNotificationService);
   private readonly router = inject(Router);
   private readonly authService = inject(AuthService);
+  protected loadingService = inject(LoadingService);
 
   loginConfig: iDynamicFormConfig[] = [
     {
@@ -54,19 +57,25 @@ export class LoginPage {
   @ViewChild(DynamicFormComponent) dynamicForm?: DynamicFormComponent;
 
   async login() {
-    if (!this.dynamicForm?.form.valid) {
-      this.notificationService.error('Erro', 'Preencha os campos corretamente');
-      return;
-    }
+    try {
+      this.loadingService.start();
 
-    const { email, password } = this.dynamicForm.form.value;
-    const { error } = await this.supabase.auth.signInWithPassword({ email, password });
-    if (error) {
-      this.notificationService.error('Erro ao fazer login', 'Verifique suas credenciais e tente novamente');
-      return;
-    }
+      if (!this.dynamicForm?.form.valid) {
+        this.notificationService.error('Erro', 'Preencha os campos corretamente');
+        return;
+      }
 
-    await this.authService.load();
-    this.router.navigate(['/']);
+      const { email, password } = this.dynamicForm.form.value;
+      const { error } = await this.supabase.auth.signInWithPassword({ email, password });
+      if (error) {
+        this.notificationService.error('Erro ao fazer login', 'Verifique suas credenciais e tente novamente');
+        return;
+      }
+
+      await this.authService.load();
+      this.router.navigate(['/']);
+    } finally {
+      this.loadingService.stop();
+    }
   }
 }
